7ce339d353740cc43d11be9e0b1390a97a55e8e7
ybbarng <ybbarng@gmail.com>
2023-03-08 17:42:14 +0900

    UseCase 내부에서만 사용하는 것들을 private field로 변경

--- web/src/lib/server/add/domain/add-use-case.ts
+++ web/src/lib/server/add/domain/add-use-case.ts
@@ -1,20 +1,20 @@
 import type { MessageQueueService } from './message-queue-service';
 import type { TicketService } from './ticket-service';
 export class AddUseCase {
-  messageQueueService: MessageQueueService;
-  ticketService: TicketService;
+  #messageQueueService: MessageQueueService;
+  #ticketService: TicketService;
 
   constructor(messageQueueService: MessageQueueService, ticketService: TicketService) {
-    this.messageQueueService = messageQueueService;
-    this.messageQueueService.registerOnResult((ticket: string, result: number) => {
+    this.#messageQueueService = messageQueueService;
+    this.#messageQueueService.registerOnResult((ticket: string, result: number) => {
       console.log('ticket: ' + ticket + ' result: ' + result);
     });
-    this.ticketService = ticketService;
+    this.#ticketService = ticketService;
   }
 
   public execute(number1: number, number2: number): string {
-    const ticket = this.ticketService.issueTicket();
-    this.messageQueueService.requestAdd(ticket, number1, number2);
+    const ticket = this.#ticketService.issueTicket();
+    this.#messageQueueService.requestAdd(ticket, number1, number2);
     return ticket;
   }
 }
