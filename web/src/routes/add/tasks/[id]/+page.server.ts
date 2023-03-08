f9695e5c85ec585de069d0e8c06256365cb9f30f
ybbarng <ybbarng@gmail.com>
2023-03-08 19:15:01 +0900

    AddTask가 없는 경우 처리

--- web/src/routes/add/tasks/[id]/+page.server.ts
+++ web/src/routes/add/tasks/[id]/+page.server.ts
@@ -1,6 +1,6 @@
-import type { PageServerLoad } from './$types'
+import type { PageServerLoad } from './$types';
 
-import { loadAddTaskUseCase } from '$lib/server/add'
+import { loadAddTaskUseCase } from '$lib/server/add';
 
 export const load: PageServerLoad = async ({params}) => {
   const addTask = loadAddTaskUseCase.execute(params.id)
--- web/src/routes/add/tasks/[id]/+page.svelte
+++ web/src/routes/add/tasks/[id]/+page.svelte
@@ -1,13 +1,20 @@
-<script>
-	/** @type {import('./$types').PageData} */
-	export let data;
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { PageData } from './$types';
+
+	export var data: PageData;
+	export const id = $page.params.id;
 </script>
 
-<h1>AddTask: {data.addTask.id}</h1>
-<ul>
-	<li>createdAt: {data.addTask.createdAt}</li>
-	<li>resultAt: {data.addTask.resultAt}</li>
-	<li>parameter1: {data.addTask.parameter1}</li>
-	<li>parameter2: {data.addTask.parameter2}</li>
-	<li>result: {data.addTask.result}</li>
-</ul>
+{#if data.addTask}
+	<h1>AddTask: {data.addTask.id}</h1>
+	<ul>
+		<li>createdAt: {data.addTask.createdAt}</li>
+		<li>resultAt: {data.addTask.resultAt}</li>
+		<li>parameter1: {data.addTask.parameter1}</li>
+		<li>parameter2: {data.addTask.parameter2}</li>
+		<li>result: {data.addTask.result}</li>
+	</ul>
+{:else}
+	<h1>AddTask with {id} is not found.</h1>
+{/if}
