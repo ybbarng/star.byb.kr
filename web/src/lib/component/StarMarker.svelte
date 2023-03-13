925a9c845aafac7af2e312652d2a0ee54c950914
ybbarng <ybbarng@gmail.com>
2023-03-13 17:32:04 +0900

    이미지를 그리고 별 위치를 정할 컴포넌트를 별도 코드로 분리

---
+++ web/src/lib/component/StarMarker.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export var imageDataUrl: string;
+  var image: HTMLImageElement;
+  export var imageWidth = 0;
+  export var imageHeight = 0;
+  const onImageLoaded = () => {
+    imageWidth = image.width;
+    imageHeight = image.height;
+  }
+  const onMouseClickedOnImage = (event) => {
+    const rectOfCanvas = image.getBoundingClientRect();
+    const x = event.clientX - rectOfCanvas.left;
+    const y = event.clientY - rectOfCanvas.top;
+    console.log(`Mouse clicked: (${x}, ${y})`)
+  }
+</script>
+
+<div class="star-marker">
+<img id="image" src={imageDataUrl} alt="the resource to be solved" draggable="false"
+  bind:this={image}
+  on:load={() => onImageLoaded()}
+  on:mousedown={(event) => onMouseClickedOnImage(event)}
+/>
+</div>
--- web/src/routes/plate-solving/+page.svelte
+++ web/src/routes/plate-solving/+page.svelte
@@ -1,24 +1,13 @@
 <script lang="ts">
   import ImageUploader from '$lib/component/ImageUploader.svelte';
-  var imageDataUrl: string;
-  var image: HTMLImageElement;
+  import StarMarker from '$lib/component/StarMarker.svelte';
   var imageDataUrl: string;
   var imageWidth = 0;
   var imageHeight = 0;
-  const onImageLoaded = () => {
-    imageWidth = image.width;
-    imageHeight = image.height;
-  }
-  const onMouseClickedOnImage = (event) => {
-    const rectOfCanvas = image.getBoundingClientRect();
-    const x = event.clientX - rectOfCanvas.left;
-    const y = event.clientY - rectOfCanvas.top;
-    console.log(`Mouse clicked: (${x}, ${y})`)
-  }
 </script>
 <h1>천체 이미지 분석</h1>
 <p>천체 이미지 분석 페이지입니다.</p>
-{#if imageDataUrl && image && imageHeight > 0}
+{#if imageDataUrl && imageHeight > 0}
 <div id="image-info">
 <ul>
   <li>이미지 가로: {imageWidth}</li>
@@ -29,22 +18,22 @@
   {#if !imageDataUrl}
     <ImageUploader bind:imageDataUrl={imageDataUrl} />
   {:else}
-    <img id="image" src={imageDataUrl} alt="image to be solved" draggable="false"
-      bind:this={image}
-      on:load={() => onImageLoaded()}
-      on:mousedown={(event) => onMouseClickedOnImage(event)}
-    />
+    <StarMarker
+      bind:imageDataUrl={imageDataUrl}
+      bind:imageWidth={imageWidth}
+      bind:imageHeight={imageHeight}
+      />
   {/if}
 </div>
 
 <style>
-    .container {
-        width: 100%;
-        min-height: 600px;
-        border: 1px black solid;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+  .container {
+    width: 100%;
+    min-height: 600px;
+    border: 1px black solid;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
