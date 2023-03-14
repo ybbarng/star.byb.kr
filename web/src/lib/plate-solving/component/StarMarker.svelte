cc101f053cdaffbd24fe5b541ca9b5d5c8576709
ybbarng <ybbarng@gmail.com>
2023-03-14 11:10:14 +0900

    업로드한 이미지를 canvas에 그리기 구현

--- web/src/lib/plate-solving/component/StarMarker.svelte
+++ web/src/lib/plate-solving/component/StarMarker.svelte
@@ -3,8 +3,16 @@
   import type { Star } from '$lib/plate-solving/value-object/star'
 
   export var imageDataUrl: string;
-  var image: HTMLImageElement;
   export var markedImage: MarkedImage;
+  var canvas: HTMLCanvasElement;
+  var image: HTMLImageElement = new Image();
+
+  const drawCanvas = (width: number, height:number) => {
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, width, height);
+  }
 
   const onImageLoaded = () => {
     markedImage = {
@@ -12,21 +20,26 @@
       height: image.height,
       stars: []
     }
+    drawCanvas(image.width, image.height);
   }
   const onMouseClickedOnImage = (event) => {
-    const rectOfCanvas = image.getBoundingClientRect();
+    const rectOfCanvas = canvas.getBoundingClientRect();
     const x = event.clientX - rectOfCanvas.left;
     const y = event.clientY - rectOfCanvas.top;
     console.log(`Mouse clicked: (${x}, ${y})`)
     markedImage.stars.push({x, y});
     markedImage['stars'] = markedImage.stars;
   }
+
+  image.src = imageDataUrl;
+  image.onload = () => {
+    onImageLoaded();
+  }
 </script>
 
 <div class="star-marker">
-<img id="image" src={imageDataUrl} alt="the resource to be solved" draggable="false"
-  bind:this={image}
-  on:load={() => onImageLoaded()}
+<canvas
+  bind:this={canvas}
   on:mousedown={(event) => onMouseClickedOnImage(event)}
 />
 </div>
