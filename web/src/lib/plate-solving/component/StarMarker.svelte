73b09563acef99ee10c24075b6bcf68a9d4a3ebe
ybbarng <ybbarng@gmail.com>
2023-03-14 11:20:25 +0900

    마우스로 클릭한 곳에 이미지 구현

--- web/src/lib/plate-solving/component/StarMarker.svelte
+++ web/src/lib/plate-solving/component/StarMarker.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
   import type { MarkedImage } from '$lib/plate-solving/value-object/marked-image';
-  import type { Star } from '$lib/plate-solving/value-object/star'
+  import type { Star } from '$lib/plate-solving/value-object/star';
+  import starImageAsset from '$lib/plate-solving/assets/star.png';
 
   export var imageDataUrl: string;
   export var markedImage: MarkedImage;
   var canvas: HTMLCanvasElement;
   var image: HTMLImageElement = new Image();
+  var starImage: HTMLImageElement = new Image();
+  starImage.src = starImageAsset;
+  console.log(starImageAsset);
 
   const drawCanvas = (width: number, height:number) => {
     canvas.width = width;
     canvas.height = height;
     const context = canvas.getContext('2d');
     context.drawImage(image, 0, 0, width, height);
+  }
+
+  const drawStarOnCanvas = (x: number, y:number) => {
+    const context = canvas.getContext('2d');
+    const starX = x - (starImage.width / 2);
+    const starY = y - (starImage.height / 2);
+    context.drawImage(starImage, starX, starY, starImage.width, starImage.height);
   }
 
   const onImageLoaded = () => {
@@ -29,6 +40,7 @@
     console.log(`Mouse clicked: (${x}, ${y})`)
     markedImage.stars.push({x, y});
     markedImage['stars'] = markedImage.stars;
+    drawStarOnCanvas(x, y);
   }
 
   image.src = imageDataUrl;
