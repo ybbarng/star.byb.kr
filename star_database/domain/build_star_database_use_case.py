d98c5ad9c489553975d9ec430fc7a4b49178520f
ybbarng <ybbarng@gmail.com>
2023-03-24 10:41:05 +0900

    N_SIDE를 11로 결정

--- star_database/domain/build_star_database_use_case.py
+++ star_database/domain/build_star_database_use_case.py
@@ -1,5 +1,3 @@
-from math import comb
-
 from domain.catalog_service import CatalogService
 from domain.coordinate_service import CoordinateService
 
@@ -10,19 +8,8 @@ class BuildStarDatabaseUseCase():
 
     def execute(self):
         print("Building star database is started.")
-        N_SIDE = 10
+        N_SIDE = 11
         stars = self.catalog_service.load()
         print(f"{len(stars)} data is loaded.")
-        for i in range(1, 17):
-            groups = self.coordinate_service.group_by_healpixes(i, stars)
-            min = 999999999
-            max = -1
-            triangles = 0
-            for group in groups:
-                group_size = len(group)
-                if group_size < min:
-                    min = group_size
-                if group_size > max:
-                    max = group_size
-                triangles += comb(group_size, 3)
-            print(f"N_SIDE: {i}, Groups: {len(groups)}, Min: {min}, Max: {max}, Triangles: {triangles}")
+        groups = self.coordinate_service.group_by_healpixes(N_SIDE, stars)
+        print(groups[0])
