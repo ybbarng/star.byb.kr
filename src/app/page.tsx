9d228f9a7be6a364437d9ec59e36cf5938e7f195
ybbarng <ybbarng@gmail.com>
2025-01-15 19:55:11 +0900

    샘플 이미지를 사용해서 흑백 이미지로 변환하는 기능 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useEffect, useRef, useState } from 'react'
+import {useRef, useState} from 'react'
 import cv from '../services/cv'
 
-// We'll limit the processing size to 200px.
-const maxVideoSize = 200
+const SAMPLE_SRC ="/samples/ursa-major.jpg";
+const SAMPLE_WIDTH =1152;
+const SAMPLE_HEIGHT =819;
 
 /**
  * What we're going to render is:
@@ -19,19 +20,27 @@ const maxVideoSize = 200
  */
 export default function Page() {
   const [processing, updateProcessing] = useState(false)
-  const videoElement = useRef(null)
-  const canvasEl = useRef(null)
+  const imageElement = useRef<HTMLImageElement>(null)
+  const canvasElement = useRef<HTMLCanvasElement>(null)
 
   /**
    * In the onClick event we'll capture a frame within
    * the video to pass it to our service.
    */
   async function onClick() {
+    if (!imageElement.current || !canvasElement.current) {
+      console.log("Can't find elements");
+      return;
+    }
     updateProcessing(true)
 
-    const ctx = canvasEl.current.getContext('2d')
-    ctx.drawImage(videoElement.current, 0, 0, maxVideoSize, maxVideoSize)
-    const image = ctx.getImageData(0, 0, maxVideoSize, maxVideoSize)
+    const ctx = canvasElement.current.getContext('2d')
+    if (!ctx) {
+      updateProcessing(false)
+      return;
+    }
+    ctx.drawImage(imageElement.current, 0, 0, SAMPLE_WIDTH, SAMPLE_HEIGHT)
+    const image = ctx.getImageData(0, 0, SAMPLE_WIDTH, SAMPLE_HEIGHT)
     // Load the model
     await cv.load()
     // Processing image
@@ -40,47 +49,6 @@ export default function Page() {
     ctx.putImageData(processedImage.data.payload, 0, 0)
     updateProcessing(false)
   }
-
-  /**
-   * In the useEffect hook we'll load the video
-   * element to show what's on camera.
-   */
-  useEffect(() => {
-    async function initCamara() {
-      videoElement.current.width = maxVideoSize
-      videoElement.current.height = maxVideoSize
-
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            facingMode: 'user',
-            width: maxVideoSize,
-            height: maxVideoSize,
-          },
-        })
-        videoElement.current.srcObject = stream
-
-        return new Promise((resolve) => {
-          videoElement.current.onloadedmetadata = () => {
-            resolve(videoElement.current)
-          }
-        })
-      }
-      const errorMessage =
-        'This browser does not support video capture, or this device does not have a camera'
-      alert(errorMessage)
-      return Promise.reject(errorMessage)
-    }
-
-    async function load() {
-      const videoLoaded = await initCamara()
-      videoLoaded.play()
-      return videoLoaded
-    }
-
-    load()
-  }, [])
 
   return (
     <div
@@ -91,18 +59,18 @@ export default function Page() {
         flexDirection: 'column',
       }}
     >
-      <video className="video" playsInline ref={videoElement} />
+      <img src={SAMPLE_SRC} width={SAMPLE_WIDTH} height={SAMPLE_HEIGHT} ref={imageElement} />
       <button
         disabled={processing}
-        style={{ width: maxVideoSize, padding: 10 }}
+        style={{width: SAMPLE_WIDTH, padding: 10}}
         onClick={onClick}
       >
         {processing ? 'Processing...' : 'Take a photo'}
       </button>
       <canvas
-        ref={canvasEl}
-        width={maxVideoSize}
-        height={maxVideoSize}
+        ref={canvasElement}
+        width={SAMPLE_WIDTH}
+        height={SAMPLE_HEIGHT}
       ></canvas>
     </div>
   )
