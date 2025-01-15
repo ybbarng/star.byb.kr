fbaea71b1d11c75692a92a2a17ca415d9ad2f6c7
ybbarng <ybbarng@gmail.com>
2025-01-15 18:48:05 +0900

    테스트용 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,109 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useRef, useState } from 'react'
+import cv from '../services/cv'
+
+// We'll limit the processing size to 200px.
+const maxVideoSize = 200
+
+/**
+ * What we're going to render is:
+ *
+ * 1. A video component so the user can see what's on the camera.
+ *
+ * 2. A button to generate an image of the video, load OpenCV and
+ * process the image.
+ *
+ * 3. A canvas to allow us to capture the image of the video and
+ * show it to the user.
+ */
+export default function Page() {
+  const [processing, updateProcessing] = useState(false)
+  const videoElement = useRef(null)
+  const canvasEl = useRef(null)
+
+  /**
+   * In the onClick event we'll capture a frame within
+   * the video to pass it to our service.
+   */
+  async function onClick() {
+    updateProcessing(true)
+
+    const ctx = canvasEl.current.getContext('2d')
+    ctx.drawImage(videoElement.current, 0, 0, maxVideoSize, maxVideoSize)
+    const image = ctx.getImageData(0, 0, maxVideoSize, maxVideoSize)
+    // Load the model
+    await cv.load()
+    // Processing image
+    const processedImage = await cv.imageProcessing(image)
+    // Render the processed image to the canvas
+    ctx.putImageData(processedImage.data.payload, 0, 0)
+    updateProcessing(false)
+  }
+
+  /**
+   * In the useEffect hook we'll load the video
+   * element to show what's on camera.
+   */
+  useEffect(() => {
+    async function initCamara() {
+      videoElement.current.width = maxVideoSize
+      videoElement.current.height = maxVideoSize
+
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            facingMode: 'user',
+            width: maxVideoSize,
+            height: maxVideoSize,
+          },
+        })
+        videoElement.current.srcObject = stream
+
+        return new Promise((resolve) => {
+          videoElement.current.onloadedmetadata = () => {
+            resolve(videoElement.current)
+          }
+        })
+      }
+      const errorMessage =
+        'This browser does not support video capture, or this device does not have a camera'
+      alert(errorMessage)
+      return Promise.reject(errorMessage)
+    }
+
+    async function load() {
+      const videoLoaded = await initCamara()
+      videoLoaded.play()
+      return videoLoaded
+    }
+
+    load()
+  }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <video className="video" playsInline ref={videoElement} />
+      <button
+        disabled={processing}
+        style={{ width: maxVideoSize, padding: 10 }}
+        onClick={onClick}
+      >
+        {processing ? 'Processing...' : 'Take a photo'}
+      </button>
+      <canvas
+        ref={canvasEl}
+        width={maxVideoSize}
+        height={maxVideoSize}
+      ></canvas>
     </div>
-  );
+  )
 }
