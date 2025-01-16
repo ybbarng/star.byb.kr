c1ff6a5c3f101252fb9323b0125e43700870708e
ybbarng <ybbarng@gmail.com>
2025-01-17 01:08:03 +0900

    별을 그리는 로직도 별도의 훅으로 분리

---
+++ src/app/database/hooks/useThreeStars.ts
@@ -0,0 +1,63 @@
+import {useEffect, useState} from "react";
+import * as THREE from "three";
+import database from "@database/build/vectors-database.json";
+
+export const useThreeStars = () => {
+  const [stars, setStars] = useState<THREE.Points | null>(null);
+
+  useEffect(() => {
+    const color = new THREE.Color().setHex( 0xFFFFFF );
+    const positions: number[] = [];
+    const colors: number[] = [];
+    const sizes: number[] = [];
+    // 2. 3D 좌표 점 생성
+    database.forEach((star) => {
+      positions.push(star.x * 100);
+      positions.push(star.y * 100);
+      positions.push(star.z * 100);
+      const size = (star.V * 26) / 255 + 0.18;
+      colors.push(color.r, color.g, color.b, size);
+      sizes.push(size);
+    })
+    const starsGeometry = new THREE.BufferGeometry();
+    starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    starsGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 4));
+    starsGeometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
+
+    const starsMaterial = new THREE.ShaderMaterial({
+      vertexShader: vertexShader(),
+      fragmentShader: fragmentShader(),
+      transparent: true,
+    })
+
+    const point = new THREE.Points(starsGeometry, starsMaterial);
+    setStars(point);
+  }, []);
+  return {
+    stars,
+  }
+}
+
+function vertexShader() {
+  return `
+    attribute float size;
+    attribute vec4 color;
+    varying vec4 vColor;
+    void main() {
+        vColor = color;
+        vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+        gl_PointSize = size * ( 250.0 / -mvPosition.z );
+        gl_Position = projectionMatrix * mvPosition;
+    }
+  `;
+}
+
+
+function fragmentShader() {
+  return `
+    varying vec4 vColor;
+        void main() {
+            gl_FragColor = vec4( vColor );
+        }
+  `;
+}
