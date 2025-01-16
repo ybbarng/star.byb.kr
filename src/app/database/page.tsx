10540cdda97fca1ef317daa2d115edede86545c7
ybbarng <ybbarng@gmail.com>
2025-01-17 00:36:22 +0900

    데이터베이스 그리는 기능 추가

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import * as THREE from "three";
 import {useEffect, useRef} from "react";
-import database from "@database/build/reduced-database.json";
-console.log(database);
+import database from "@database/build/vectors-database.json";
 
 export default function Page() {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -13,24 +12,43 @@ export default function Page() {
     }
     // 1. 씬, 카메라, 렌더러 설정
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
 
+    const color = new THREE.Color().setHex( 0xFFFFFF );
+    const positions: number[] = [];
+    const colors: number[] = [];
+    const sizes: number[] = [];
     // 2. 3D 좌표 점 생성
-    const geometry = new THREE.SphereGeometry(0.1, 32, 32); // 점을 작은 구로 만듦
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const point = new THREE.Mesh(geometry, material);
+    database.forEach((star) => {
+      positions.push(star.x * 100);
+      positions.push(star.y * 100);
+      positions.push(star.z * 100);
+      const size = (star.V * 26) / 255 + 0.18;
+      colors.push(color.r, color.g, color.b, size);
+      sizes.push(size);
+    })
+    const starsGeometry = new THREE.BufferGeometry();
+    starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    starsGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 4));
+    starsGeometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
 
-    // 3D 좌표 값 설정 (예: (x, y, z) = (2, 3, -5))
-    point.position.set(2, 3, -5);
+    const starsMaterial = new THREE.ShaderMaterial({
+      vertexShader: vertexShader(),
+      fragmentShader: fragmentShader(),
+      transparent: true,
+    })
+
+    const point = new THREE.Points(starsGeometry, starsMaterial);
 
     // 점을 씬에 추가
     scene.add(point);
 
+
     // 카메라 위치 설정
-    camera.position.z = 5;
+    camera.position.z = 200;
 
     // 애니메이션 함수
     const animate = () => {
@@ -47,4 +65,28 @@ export default function Page() {
   }, [mountRef.current]);
 
   return <div ref={ mountRef } />
+}
+
+function vertexShader() {
+  return `
+    attribute float size;
+    attribute vec4 color;
+    varying vec4 vColor;
+    void main() {
+        vColor = color;
+        vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+        gl_PointSize = size * ( 250.0 / -mvPosition.z );
+        gl_Position = projectionMatrix * mvPosition;
+    }
+  `;
+}
+
+
+function fragmentShader() {
+  return `
+    varying vec4 vColor;
+        void main() {
+            gl_FragColor = vec4( vColor );
+        }
+  `;
 }
