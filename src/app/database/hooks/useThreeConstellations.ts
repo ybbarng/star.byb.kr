e651188fa093083b091f5be9818d66e89e575c32
ybbarng <ybbarng@gmail.com>
2025-01-17 01:40:05 +0900

    별자리를 그리기 위한 데이터 준비

---
+++ src/app/database/hooks/useThreeConstellations.ts
@@ -0,0 +1,113 @@
+import {useEffect, useState} from "react";
+import * as THREE from "three";
+
+export const useThreeConstellations = () => {
+  const [constellations, setConstellations] = useState<THREE.Line[]>([]);
+
+  const loadStarCatalog = async () => {
+    const response = await fetch("/catalogs/bsc5.dat");
+    if (!response.ok) {
+      throw new Error("별 정보를 로드하지 못했습니다.");
+    }
+    const bsc5dat = await response.text();
+    const starData = bsc5dat.split('\n');
+    const starCatalog: StarCatalog = {}
+    starData.forEach((row: string) => {
+      const star = {
+        id: Number(row.slice(0, 4)),
+        name: row.slice(4, 14).trim(),
+        gLon: Number(row.slice(90, 96)),
+        gLat: Number(row.slice(96, 102)),
+        mag: Number(row.slice(102, 107)),
+        spectralClass: row.slice(129, 130),
+        v: new THREE.Vector3(),
+      }
+      star.v = new THREE.Vector3().setFromSphericalCoords(
+        100,
+        ((90 - star.gLat) / 180) * Math.PI,
+        (star.gLon / 180) * Math.PI
+      )
+      starCatalog[star.id] = star
+    });
+    return starCatalog;
+  }
+
+  const loadConstellationData = async () => {
+    const response = await fetch("/catalogs/ConstellationLines.dat")
+    if (!response.ok) {
+      throw new Error("별자리 정보를 로드하지 못했습니다.");
+    }
+    const data = await response.text();
+
+    const constellationLinesData = data.split('\n');
+    const constellations: ConstellationLineData[] = [];
+    constellationLinesData.forEach((row) => {
+      if (!row.startsWith('#') && row.length > 1) {
+        const rowData = row.split(/[ ,]+/)
+        const starIds = []
+        for (let i = 0; i < rowData.length - 2; i++) {
+          const starId = parseInt(rowData[i + 2].trim());
+          starIds.push(starId);
+        }
+        constellations.push({
+          starIds,
+        })
+      }
+    });
+    return constellations;
+  }
+
+  useEffect(() => {
+    const createThreeConstellations = async () => {
+
+      const starCatalog = await loadStarCatalog();
+      const constellationData = await loadConstellationData();
+
+      const constellations: THREE.Line[] = [];
+      constellationData.forEach((constellation) => {
+        const points: THREE.Vector3[] = [];
+        constellation.starIds.forEach((starId: number) => {
+          if (starId in starCatalog) {
+            const star = starCatalog[starId];
+            points.push(star.v)
+          }
+        });
+        const constellationGeometry = new THREE.BufferGeometry().setFromPoints(
+          points
+        )
+        const constellationMaterial = new THREE.LineBasicMaterial({
+          color: 0x008888,
+        })
+        const constellationLine = new THREE.Line(
+          constellationGeometry,
+          constellationMaterial
+        )
+        constellationLine.userData.type = 'constellationLine'
+        constellations.push(constellationLine);
+      });
+      setConstellations(constellations);
+    }
+    createThreeConstellations();
+  }, []);
+  return {
+    constellations,
+  }
+}
+
+interface Star {
+  id: number;
+  name: string;
+  gLon: number;
+  gLat: number;
+  mag: number;
+  spectralClass: string;
+  v: THREE.Vector3;
+}
+
+interface StarCatalog {
+  [key: number]: Star
+}
+
+interface ConstellationLineData {
+  starIds: number[];
+}
--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -2,11 +2,13 @@
 import {useEffect, useRef } from "react";
 import {useThreeScene} from "@/app/database/hooks/useThreeScene";
 import {useThreeStars} from "@/app/database/hooks/useThreeStars";
+import {useThreeConstellations} from "@/app/database/hooks/useThreeConstellations";
 
 export default function Page() {
   const mountRef = useRef<HTMLDivElement>(null);
   const { scene } = useThreeScene(mountRef);
   const { stars } = useThreeStars();
+  const { constellations } = useThreeConstellations();
 
   useEffect(() => {
     if (!scene || !stars) {
