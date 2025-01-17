6493821b47579d91a422cb0a8374525ded1d0518
ybbarng <ybbarng@gmail.com>
2025-01-17 23:24:43 +0900

    healpix 경계선 그리는 로직 구현

---
+++ src/app/database/hooks/useThreeHealpix.ts
@@ -0,0 +1,45 @@
+import {useEffect, useState} from "react";
+import * as THREE from "three";
+import * as healpix from '@hscmap/healpix';
+
+export const useThreeHealpix = () => {
+  const nstep = 8; // 경계선의 한 선분을 구성할 점의 갯수
+  const nside = 8; // healpix의 order. astrometry에서는 12 썼는데, 라이브러리의 버그인지 여기서는 2의 배수여야 각 영역이 깔끔하게 분리됨
+  const npix = 12 * nside * nside;
+  const R = 100;
+
+  const [borders, setBorders] = useState<THREE.Line[]>([]);
+
+  useEffect(() => {
+    const borders: THREE.Line[] = [];
+    for (let ipix = 0; ipix < npix; ++ipix) {
+      const path: number[] = [];
+      for (let i = 0; i < nstep; i++) {
+        const ne = i / nstep;
+        const v = healpix.pixcoord2vec_nest(nside, ipix, ne, 0);
+        path.push(v[0] * R);
+        path.push(v[2] * R);
+        path.push(v[1] * R);
+      }
+      for (let i = 0; i <= nstep; i++) {
+        const nw = i / nstep;
+        const v = healpix.pixcoord2vec_nest(nside, ipix, 1, nw);
+        path.push(v[0] * R);
+        path.push(v[2] * R);
+        path.push(v[1] * R);
+      }
+      const geometry = new THREE.BufferGeometry();
+      geometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute(path, 3)
+      );
+      const material = new THREE.LineBasicMaterial({color: 0x1e40af});
+      const border = new THREE.Line(geometry, material);
+      borders.push(border);
+    }
+    setBorders(borders);
+  }, []);
+  return {
+    borders
+  };
+}
--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -3,13 +3,16 @@ import {useEffect, useRef, useState} from "react";
 import {useThreeScene} from "@/app/database/hooks/useThreeScene";
 import {useThreeStars} from "@/app/database/hooks/useThreeStars";
 import {useThreeConstellations} from "@/app/database/hooks/useThreeConstellations";
+import {useThreeHealpix} from "@/app/database/hooks/useThreeHealpix";
 
 export default function Page() {
   const mountRef = useRef<HTMLDivElement>(null);
   const [showConstellations, setShowConstellations] = useState(false);
+  const [showHealpix, setShowHealpix] = useState(false);
   const { scene } = useThreeScene(mountRef);
   const { stars } = useThreeStars();
   const { constellations } = useThreeConstellations();
+  const { borders: healpixBorders } = useThreeHealpix();
 
   useEffect(() => {
     if (!scene || !stars) {
@@ -33,18 +36,43 @@ export default function Page() {
     })
   }, [scene, constellations, showConstellations]);
 
+  useEffect(() => {
+    if (!scene || !healpixBorders) {
+      return;
+    }
+    if (showHealpix) {
+      healpixBorders.forEach((border) => {
+        scene.add(border);
+      })
+      return;
+    }
+    healpixBorders.forEach((border) => {
+      scene.remove(border);
+    })
+  }, [scene, healpixBorders, showHealpix]);
+
   return <div className="relative">
     <div ref={ mountRef } />
 
-    <div className="absolute top-4 left-4 flex flex-row gap-1">
+    <div className="absolute top-4 left-4 flex flex-row gap-4">
       <div className="flex items-center mb-4">
-        <input id="default-checkbox"
+        <input id="show-constellations"
                type="checkbox"
                checked={showConstellations}
                onChange={(event) => setShowConstellations(event.target.checked)}
                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-        <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+        <label htmlFor="show-constellations" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
           별자리 그리기
+        </label>
+      </div>
+      <div className="flex items-center mb-4">
+        <input id="show-healpix"
+               type="checkbox"
+               checked={showHealpix}
+               onChange={(event) => setShowHealpix(event.target.checked)}
+               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+        <label htmlFor="show-healpix" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Healpix 그리기
         </label>
       </div>
     </div>
