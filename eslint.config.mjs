3d9024f0be056c6766c2e7026f3ed89d7c40c810
ybbarng <ybbarng@gmail.com>
2025-01-21 22:40:42 +0900

    import lint 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -34,8 +35,37 @@ const eslintConfig = [
           "newlines-between": "never",
         },
       ],
-    }
-  }
+      "no-extra-boolean-cast": "warn",
+      "newline-before-return": "error",
+      "comma-dangle": ["error", "always-multiline"],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "function", next: "*" },
+        { blankLine: "always", prev: "*", next: "function" },
+        { blankLine: "always", prev: "*", next: "multiline-block-like" },
+        { blankLine: "always", prev: "multiline-block-like", next: "*" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prettier/prettier": [
+        "error",
+        {
+          doubleQuote: true,
+          plugins: ["prettier-plugin-tailwindcss"],
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      "no-relative-import-paths": noRelativeImportPaths,
+    },
+    rules: {
+      "no-relative-import-paths/no-relative-import-paths": [
+        "warn",
+        { allowSameFolder: true, rootDir: "src", prefix: "@" },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
