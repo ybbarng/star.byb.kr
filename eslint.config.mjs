1fc330ce10a6a3a5a1caa8917ab4b055116ef1ca
ybbarng <ybbarng@gmail.com>
2025-01-21 22:24:40 +0900

    prettier를 eslint에 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  eslintPluginPrettierRecommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
