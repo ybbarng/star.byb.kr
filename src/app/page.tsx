d1ced499827b4edd076d8ee94c4ce1c20305b470
ybbarng <ybbarng@gmail.com>
2025-01-24 00:43:03 +0900

    잘못된 조건문 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Page() {
       }
     });
 
-    if (candidates.length < 1 || !selectedCandidateIndex) {
+    if (candidates.length < 1 || selectedCandidateIndex === undefined) {
       return;
     }
 
