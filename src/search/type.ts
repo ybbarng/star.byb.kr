b4fe0ac9def0875613a3e9de18db3a8732b8c152
ybbarng <ybbarng@gmail.com>
2025-01-23 22:39:27 +0900

    candidate를 찾는 로직을 worker로 옮겨서 성능 개선

--- src/search/type.ts
+++ src/search/type.ts
@@ -5,3 +5,23 @@ export interface Photo {
   height: number;
   stars: Point2D[];
 }
+
+export type CandidateInput = [number, number, number, number];
+
+export interface CandidateOutputItem {
+  hr: number;
+  label: string;
+}
+
+export type CandidateOutput = [
+  CandidateOutputItem,
+  CandidateOutputItem,
+  CandidateOutputItem,
+  CandidateOutputItem,
+];
+
+export interface Candidate {
+  input: CandidateInput;
+  output: CandidateOutput;
+  distance: number;
+}
--- src/search/workers/findCandidatesWorker.ts
+++ src/search/workers/findCandidatesWorker.ts
@@ -1,26 +1,102 @@
-const test = async () => {
-  const { promise, resolve } = Promise.withResolvers<void>();
-  let counter = 0;
-  const worker = setInterval(() => {
-    if (counter > 10) {
-      console.log("Worker: ", "done");
-      clearInterval(worker);
-      resolve();
+import { Star } from "@/scripts/database/types";
+import * as hashLib from "@/scripts/hash/hash";
+import * as quadrilateral from "@/scripts/hash/quadrilateral";
+import { Hash, NamedQuadrilateral2D } from "@/scripts/hash/types";
+import {
+  Candidate,
+  CandidateInput,
+  CandidateOutput,
+  Photo,
+} from "@/search/type";
+import catalog from "@build/database/reduced-database.json";
+import _hashes from "@build/hash/hashed-database.json";
+const hashes = _hashes as HashedQuad[];
+
+interface HashedQuad {
+  hash: Hash;
+  stars: [number, number, number, number];
+}
+
+const dictionary = new Map();
+catalog.map((star: Star) => {
+  dictionary.set(star.HR, star.N);
+});
+
+const zip = (arr1: number[], arr2: number[]) => {
+  return arr1.map((value, index) => [value, arr2[index]]);
+};
+
+const calculateDistance = (v1: number[], v2: number[]) => {
+  return zip(v1, v2)
+    .map(([a, b]) => Math.pow(a - b, 2))
+    .reduce((sum, a) => sum + a, 0);
+};
+
+const findCandidates = (photo: Photo) => {
+  const quads = quadrilateral.create(photo.stars).map((quadrilateral) => {
+    const hash = hashLib.calculateHash(
+      quadrilateral.stars.map((vector, i) => ({
+        label: String(i),
+        vector,
+      })) as NamedQuadrilateral2D,
+    );
+
+    return {
+      labels: hash.labels,
+      hash: hash.hash,
+    };
+  });
+
+  const candidates: Candidate[] = [];
+
+  quads.forEach((quad) => {
+    let minDistance = Number.MAX_VALUE;
+    let minIndex = -1;
+
+    for (let i = 0; i < hashes.length; i += 1) {
+      const index = hashes[i];
+      const distance = calculateDistance(index.hash, quad.hash);
+
+      if (minDistance < distance) {
+        continue;
+      }
+
+      minDistance = distance;
+      minIndex = i;
     }
 
-    console.log("Worker: ", counter);
+    if (minIndex === -1) {
+      console.log("검색 결과가 없습니다.");
 
-    counter++;
-  }, 1000);
+      return;
+    }
 
-  return promise;
+    const found = hashes[minIndex].stars;
+
+    const getName = (hr: number) => {
+      const name = dictionary.get(hr);
+
+      return name ? name : `HR ${hr}`;
+    };
+
+    candidates.push({
+      input: quad.labels.map((label) => Number(label)) as CandidateInput,
+      output: found.map((hr) => ({
+        hr,
+        label: getName(hr),
+      })) as CandidateOutput,
+      distance: minDistance * 1000,
+    });
+  });
+
+  return candidates.sort((ca, cb) => ca.distance - cb.distance);
 };
 
 onmessage = async function (messageEvent) {
   switch (messageEvent.data.fn) {
-    case "test": {
-      await test();
-      postMessage({ msg: messageEvent.data.fn });
+    case "findCandidates": {
+      const result = findCandidates(messageEvent.data.payload);
+      postMessage({ fn: "onCandidatesFound", payload: result });
       break;
     }
 
