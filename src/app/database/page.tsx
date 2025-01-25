338d1aa5122ddb8b5acbf49cc4e42687d44b8da4
ybbarng <ybbarng@gmail.com>
2025-01-25 16:10:50 +0900

    tailwindcss를 도구를 사용해서 4.0.0으로 업그레이드

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -81,7 +81,7 @@ export default function Page() {
             type="checkbox"
             checked={showConstellations}
             onChange={(event) => setShowConstellations(event.target.checked)}
-            className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            className="h-4 w-4 rounded-sm border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
           />
           <label
             htmlFor="show-constellations"
@@ -96,7 +96,7 @@ export default function Page() {
             type="checkbox"
             checked={showHealpix}
             onChange={(event) => setShowHealpix(event.target.checked)}
-            className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            className="h-4 w-4 rounded-sm border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
           />
           <label
             htmlFor="show-healpix"
