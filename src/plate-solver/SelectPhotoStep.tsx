e2c5ddfe8a2e17d7fac16d523df44d231337a444
ybbarng <ybbarng@gmail.com>
2025-01-25 19:12:32 +0900

    샘플 이미지 목록 선택기 구현

--- src/plate-solver/SelectPhotoStep.tsx
+++ src/plate-solver/SelectPhotoStep.tsx
@@ -1,8 +1,16 @@
+import { ChangeEvent, useState } from "react";
+import samples from "@/services/samples";
+
 export default function SelectPhotoStep() {
+  const [selectedSampleId, setSelectedSampleId] = useState(0);
+
   return (
     <div className="flex w-full flex-col lg:flex-row">
       <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
-        <SampleSelect />
+        <SampleSelect
+          selectedSampleId={selectedSampleId}
+          setSelectedSampleId={setSelectedSampleId}
+        />
       </div>
       <div className="divider lg:divider-horizontal">또는</div>
       <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
@@ -12,21 +20,34 @@ export default function SelectPhotoStep() {
   );
 }
 
-function SampleSelect() {
+interface SampleSelectProps {
+  selectedSampleId: number;
+  setSelectedSampleId: (sampleId: number) => void;
+}
+
+function SampleSelect({
+  selectedSampleId,
+  setSelectedSampleId,
+}: SampleSelectProps) {
+  function handleSelectChange(event: ChangeEvent<HTMLSelectElement>) {
+    setSelectedSampleId(Number(event.target.value));
+  }
+
   return (
     <label className="form-control w-full max-w-xs">
       <div className="label">
         <span className="label-text font-bold">샘플 사진을 선택하세요.</span>
       </div>
-      <select className="select select-bordered select-primary">
-        <option disabled selected>
-          Pick one
-        </option>
-        <option>Star Wars</option>
-        <option>Harry Potter</option>
-        <option>Lord of the Rings</option>
-        <option>Planet of the Apes</option>
-        <option>Star Trek</option>
+      <select
+        className="select select-bordered select-primary"
+        onChange={handleSelectChange}
+        value={selectedSampleId}
+      >
+        {samples.map((sample, i) => (
+          <option key={i} value={i}>
+            {sample.src.split("/").pop()}
+          </option>
+        ))}
       </select>
     </label>
   );
