818427b06bee51640fc57148b87046a3376b4cf5
ybbarng <ybbarng@gmail.com>
2025-01-25 17:49:52 +0900

    plate-solver를 위한 페이지 추가

---
+++ src/app/plate-solver/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <ul className="steps">
+        <li className="step step-primary">사진 선택</li>
+        <li className="step step-primary">별 감지</li>
+        <li className="step">후보 찾기</li>
+        <li className="step">주변 별 표시</li>
+      </ul>
+      <div>Plate solver!</div>
+    </div>
+  );
+}
