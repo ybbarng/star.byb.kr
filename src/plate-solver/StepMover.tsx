7c87b694b3ffe22ce52d035e4d4ce5f52c9751c8
ybbarng <ybbarng@gmail.com>
2025-01-25 19:54:44 +0900

    단계 이동 기능 구현

--- src/plate-solver/StepMover.tsx
+++ src/plate-solver/StepMover.tsx
@@ -1,15 +1,30 @@
+import { useStepsStore } from "@/plate-solver/store/steps";
+
 interface Props {
   disablePrev?: boolean;
   disableNext?: boolean;
 }
 
 export default function StepMover(props: Props) {
+  const current = useStepsStore((state) => state.current);
+  const steps = useStepsStore((state) => state.steps);
+  const moveToPrev = useStepsStore((state) => state.moveToPrev);
+  const moveToNext = useStepsStore((state) => state.moveToNext);
+
   return (
     <div className="flex flex-row justify-between">
-      <button className="btn btn-lg" disabled={props.disablePrev}>
+      <button
+        className="btn btn-lg"
+        disabled={props.disablePrev || current <= 0}
+        onClick={moveToPrev}
+      >
         이전
       </button>
-      <button className="btn btn-lg btn-primary" disabled={props.disableNext}>
+      <button
+        className="btn btn-lg btn-primary"
+        disabled={props.disableNext || current >= steps.length - 1}
+        onClick={moveToNext}
+      >
         다음
       </button>
     </div>
