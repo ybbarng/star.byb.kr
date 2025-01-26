816f5eb87ff4ca009e3bb118b800d79da9210c62
ybbarng <ybbarng@gmail.com>
2025-01-26 18:55:23 +0900

    후보 선택 기능 구현

--- src/plate-solver/ChooseCandidateStep.tsx
+++ src/plate-solver/ChooseCandidateStep.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import StepMover from "@/plate-solver/StepMover";
 import { useContextStore } from "@/plate-solver/store/context";
 import useFindCandidates from "@/search/hooks/useFindCandidates";
+import { cn } from "@/utils/cn";
 
 export default function ChooseCandidateStep() {
   const image = useContextStore((state) => state.image);
@@ -11,6 +12,11 @@ export default function ChooseCandidateStep() {
     number | undefined
   >(undefined);
   const { find: findCandidates, candidates } = useFindCandidates();
+  const candidateNames = useMemo(() => {
+    return candidates.map((candidate) => {
+      return candidate.output.map((star) => `[${star.label}]`).join("-");
+    });
+  }, [candidates]);
 
   useEffect(() => {
     if (!image || photoStars.length < 1) {
@@ -20,7 +26,7 @@ export default function ChooseCandidateStep() {
     findCandidates({
       width: image.width,
       height: image.height,
-      stars: photoStars.map((star) => [star.x, star.y]),
+      stars: photoStars.slice(0, 10).map((star) => [star.x, star.y]),
     });
   }, [image, photoStars]);
 
@@ -103,18 +109,60 @@ export default function ChooseCandidateStep() {
 
   return (
     <div className="flex w-full flex-col gap-4">
-      <div className="flex justify-center">
-        <canvas
-          className="max-h-[800px]"
-          ref={canvasElement}
-          width={image.width}
-          height={image.height}
-          style={{
-            aspectRatio: image.width / image.height,
-          }}
+      <div className="flex flex-row">
+        <CandidateSelect
+          selectedCandidateIndex={selectedCandidateIndex || -1}
+          setSelectedCandidateIndex={setSelectedCandidateIndex}
+          candidates={candidateNames}
         />
+        <div className="flex grow justify-center">
+          <canvas
+            className="max-h-[800px]"
+            ref={canvasElement}
+            width={image.width}
+            height={image.height}
+            style={{
+              aspectRatio: image.width / image.height,
+            }}
+          />
+        </div>
       </div>
       <StepMover disableNext={true} onBeforeNext={onBeforeNext} />
     </div>
+  );
+}
+
+interface CandidateSelectProps {
+  selectedCandidateIndex: number;
+  setSelectedCandidateIndex: (selectedCandidateIndex: number) => void;
+  candidates: string[];
+}
+
+function CandidateSelect(props: CandidateSelectProps) {
+  if (props.candidates.length < 1) {
+    return (
+      <div className="bg-base-200 rounded-box flex h-[800px] w-100 flex-row items-center justify-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="menu bg-base-200 rounded-box h-[800px] w-100 flex-row overflow-x-clip overflow-y-scroll">
+      {props.candidates.map((candidate, i) => {
+        return (
+          <li key={candidate} className="w-full">
+            <a
+              className={cn(
+                props.selectedCandidateIndex === i && "menu-active",
+              )}
+              onClick={() => props.setSelectedCandidateIndex(i)}
+            >
+              {`${i}: ${candidate}`}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
