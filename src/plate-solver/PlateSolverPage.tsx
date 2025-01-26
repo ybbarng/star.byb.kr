47f1b29e47742620873cf8051f7cf4a2a5cf4fee
ybbarng <ybbarng@gmail.com>
2025-01-26 17:42:19 +0900

    최상위 페이지에서 opencv를 로드 완료해서 중간에 이상한 상태가 없도록 함

--- src/plate-solver/PlateSolverPage.tsx
+++ src/plate-solver/PlateSolverPage.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
 import PageBody from "@/plate-solver/PageBody";
 import PageHeader from "@/plate-solver/PageHeader";
+import cv from "@/services/cv";
 
 export default function PlateSolverPage() {
+  useEffect(() => {
+    const init = async () => {
+      await cv.load();
+    };
+
+    init();
+  }, []);
+
   return (
     <div className="flex flex-col gap-6">
       <PageHeader />
