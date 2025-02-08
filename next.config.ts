681d4a8315fb8d4fe36a2e4f2e9af4fefa7e0e37
ybbarng <ybbarng@gmail.com>
2025-02-08 20:06:51 +0900

    konva를 사용할 때 빌드가 되지 않는 문제 해결

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
+
+    return config;
+  },
 };
 
 export default nextConfig;
