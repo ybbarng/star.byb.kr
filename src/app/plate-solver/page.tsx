a949ce746da7f2c7b359ef443af26ccc9b678269
ybbarng <ybbarng@gmail.com>
2025-02-08 17:15:35 +0900

    주입된 데모에서 여백이 있으면 이상하게 보이므로 plate-solver 페이지에만 여백 추가

--- src/app/plate-solver/page.tsx
+++ src/app/plate-solver/page.tsx
@@ -1,5 +1,9 @@
 import PlateSolverPage from "@/plate-solver/PlateSolverPage";
 
 export default function Page() {
-  return <PlateSolverPage />;
+  return (
+    <div className="size-full p-8">
+      <PlateSolverPage />
+    </div>
+  );
 }
